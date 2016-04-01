00deb6a821fde88ed0bfce6be17aff52e52e26cc
Dawid Kotarba <dawid.kotarba@gmail.com>
2016-04-01 18:56:07 +0200

    add create_folder.sh

---
+++ create_folder.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+DESTINATION=$1
+DESTINATION_RIGHTS=$2
+
+if [ ! -d "$DESTINATION" ]; then
+  echo "Folder $DESTINATION is not present. Creating a folder..."
+  mkdir $DESTINATION
+  chmod $DESTINATION_RIGHTS $DESTINATION
+  echo "Folder created."
+ fi
