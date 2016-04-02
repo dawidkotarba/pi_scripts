65fca6b5ada71ed314774f76da3a97c98cb80d05
Dawid Kotarba <dawid.kotarba@gmail.com>
2016-04-03 00:01:39 +0200

    update check devices

--- check_devices.py
+++ check_devices.py
@@ -2,10 +2,20 @@
 
 import check_host
 
-def check_devices(host):
- if check_host.is_pingable(host) == True:
-  print("pingable")
- else:
-  print("unreach")
+devices = {'router': '192.168.7.77',
+           'isp': '192.168.0.1',
+           'osmc': '192.168.7.79',
+           'komp': '192.168.7.80',
+           'bridge': '192.168.7.88'}
 
-check_devices("192.168.7.80")
+def check_devices(hostmap):
+
+ for k, v in hostmap.iteritems():
+
+  if check_host.is_pingable(v) == True:
+   print(k, ": pingable")
+  else:
+   print(k, ": unreachable")
+
+
+check_devices(devices)
